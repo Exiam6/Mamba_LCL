f81b75e556698f7f649da69a768815941af092a2
Exiam <121872598+Exiam6@users.noreply.github.com>
2024-05-16 01:40:24 +0800

    Update Dataset.py

--- Dataset.py
+++ Dataset.py
@@ -97,3 +97,107 @@ class NarrativeQADataset_Val(Dataset):
             'attention_mask': encoding['attention_mask'].squeeze(0),
             'answer_ids': answer_encoding['input_ids'].squeeze(0)
         }
+
+
+def load_narrativeqa_dataset(split):
+    dataset = load_dataset('narrativeqa', split=split)
+    return dataset
+
+class NarrativeQADataset(Dataset):
+    def __init__(self, data, tokenizer, max_length=512):
+        self.tokenizer = tokenizer
+        self.data = data
+        self.max_length = max_length
+        if self.tokenizer.pad_token is None:
+            self.tokenizer.pad_token = self.tokenizer.eos_token
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        item = self.data[idx]
+
+        context = item['document']['summary']['text']
+
+        question = item['question']['text']
+
+        answer = item['answers'][1]['text']
+        #print(answer)
+        # [CLS] question [SEP] context [SEP]
+        encoding = self.tokenizer.encode_plus(
+            question,
+            context,
+            add_special_tokens=True,
+            max_length=self.max_length,
+            padding='max_length',
+            truncation=True,
+            return_tensors='pt',
+            return_overflowing_tokens=False
+        )
+
+        # Tokenize the answer separately
+        answer_encoding = self.tokenizer.encode_plus(
+            answer,
+            add_special_tokens=True,
+            max_length=self.max_length,
+            padding='max_length',
+            truncation=True,
+            return_tensors='pt',
+            return_overflowing_tokens=False
+        )
+
+        return {
+            'input_ids': encoding['input_ids'].squeeze(0),
+            'attention_mask': encoding['attention_mask'].squeeze(0),
+            'answer_ids': answer_encoding['input_ids'].squeeze(0)
+        }
+
+class NarrativeQADataset_Val(Dataset):
+    def __init__(self, data, tokenizer, max_length=512):
+        self.tokenizer = tokenizer
+        self.data = data
+        self.max_length = max_length
+        if self.tokenizer.pad_token is None:
+            self.tokenizer.pad_token = self.tokenizer.eos_token
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        item = self.data[idx]
+
+        context = item['document']['text']
+
+        question = item['question']['text']
+
+        answer = item['answers'][0]['text']
+
+        # Tokenize context and question together as single input sequence
+        # [CLS] question [SEP] context [SEP]
+        encoding = self.tokenizer.encode_plus(
+            question,
+            context,
+            add_special_tokens=True,
+            max_length=self.max_length,
+            padding='max_length',
+            truncation=True,
+            return_tensors='pt',
+            return_overflowing_tokens=False
+        )
+
+        # Tokenize the answer separately
+        answer_encoding = self.tokenizer.encode_plus(
+            answer,
+            add_special_tokens=True,
+            max_length=self.max_length,
+            padding='max_length',
+            truncation=True,
+            return_tensors='pt',
+            return_overflowing_tokens=False
+        )
+
+        return {
+            'input_ids': encoding['input_ids'].squeeze(0),
+            'attention_mask': encoding['attention_mask'].squeeze(0),
+            'answer_ids': answer_encoding['input_ids'].squeeze(0)
+        }
